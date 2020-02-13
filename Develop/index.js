f654dd0b8cce12903bb61f5d9c560e501dccaac9
Alek <56727628+aseawright1@users.noreply.github.com>
2020-02-12 22:15:07 -0600

    enable data pull from github. pdf converter not functioning correctly now...

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,22 @@ const inquirer = require("inquirer")
 const generatePDF = require('html-pdf')
 const axios = require('axios')
 
+// define data parameters
+const dataParams = {
+    favcolor: '',
+    avi: '',
+    realName: '',
+    business: '',
+    location: '',
+    profileLink: '',
+    blog: '',
+    bio: '',
+    pubRepos: '',
+    followers: '',
+    stars: '',
+    following: ''
+}
+
 // begin promise
 // const writeFileAsync = util.promisify(fs.writeFile)
 
@@ -18,26 +34,56 @@ function createFile(fileName, html) {
     fs.writeFile('profile.pdf', html, (err) => {
         if (err) throw err;
     })
+
+    // const options = { format: 'A4' }
+
+    generatePDF.create(html, options).toFile('./profile.pdf', function(err, res) {
+        if (err) return console.log(err);
+        console.log('PDF generated');
+    })
 }
 
-// const options = { format: 'A4' }
-
-generatePDF.create(html, options).toFile('./profile.pdf', function(err, res) {
-    if (err) return console.log(err);
-    console.log('PDF generated');
-})
-
 // display prompts on console
-async function init() {
-    console.log('Please answer the prompts below to generate your profile PDF')
-    try {
-        const data = await promptUser()
-        const html = generateHTML.data
-        await writeFileAsync('index.html', html)
-        console.log('Successfully wrote to index.html')
-    } catch (err) {
-        console.log(err);
-    }
+// async function init() {
+//     console.log('Please answer the prompts below to generate your profile PDF')
+//     try {
+//         const data = await promptUser()
+//         const html = generateHTML.data
+//         await writeFileAsync('index.html', html)
+//         console.log('Successfully wrote to index.html')
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+
+function init() {
+    // ask the questions
+    inquirer.prompt(generateHTML.questionsquestions).then(
+        function(data) {
+            // link answers to HTML data
+            dataParams.favcolor = data.color
+            dataParams.profile = data.username
+
+            // pull data from github and link to my parameters
+            axios.get(`https://api.github.com/users/${data.username}`).then(function(response) {
+                if (response.status == 200) {
+                    dataParams.avi = response.data.avatar_url
+                    dataParams.realName = response.data.name
+                    dataParams.business = response.data.company
+                    dataParams.location = response.data.location
+                    dataParams.profileLink = response.data.html_url
+                    dataParams.blog = response.data.blog
+                    dataParams.bio = response.data.bio
+                    dataParams.pubRepos = response.data.public_repos
+                    dataParams.followers = response.data.followers
+                    dataParams.stars = response.data.public_gists
+                    dataParams.following = response.data.following
+                }
+            })
+
+
+        }
+    )
 }
 
 init();
