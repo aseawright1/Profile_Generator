ac9b498cadd426c59b17011b00b5d030492becac
Alek <56727628+aseawright1@users.noreply.github.com>
2020-02-12 19:13:06 -0600

    insert pdf creation tool

--- Develop/index.js
+++ Develop/index.js
@@ -1,26 +1,38 @@
-// import { generateHTML } from './generateHTML'
+// import utilities and references
 const generateHTML = require('./generateHTML.js');
-// let createHTML = generateHTML.generateHTML();
-
-// import utilities
-const inquirer = require("inquirer")
 const fs = require("fs")
-const util = require("util")
+const inquirer = require("inquirer")
+const generatePDF = require('html-pdf')
+const axios = require('axios')
 
 // begin promise
-const writeFileAsync = util.promisify(fs.writeFile)
+// const writeFileAsync = util.promisify(fs.writeFile)
 
 // import questions into inquirer
-function promptUser() {
-    return inquirer.prompt(generateHTML.questions)
+// function promptUser() {
+//     return inquirer.prompt(generateHTML.questions)
+// }
+
+function createFile(fileName, html) {
+    console.log('Please wait')
+    fs.writeFile('profile.pdf', html, (err) => {
+        if (err) throw err;
+    })
 }
+
+// const options = { format: 'A4' }
+
+generatePDF.create(html, options).toFile('./profile.pdf', function(err, res) {
+    if (err) return console.log(err);
+    console.log('PDF generated');
+})
 
 // display prompts on console
 async function init() {
     console.log('Please answer the prompts below to generate your profile PDF')
     try {
         const data = await promptUser()
-        const html = generateHTML(data)
+        const html = generateHTML.data
         await writeFileAsync('index.html', html)
         console.log('Successfully wrote to index.html')
     } catch (err) {
