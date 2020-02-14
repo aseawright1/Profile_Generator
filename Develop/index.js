0fe2b3d16cfd34e5c2b5c23716d9d222108b9d22
Alek <56727628+aseawright1@users.noreply.github.com>
2020-02-14 12:54:24 -0600

    troubleshooting TypeError undefined..

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,13 @@
 // import utilities and references
-const generateHTML = require('./generateHTML.js');
-const fs = require("fs")
-const inquirer = require("inquirer")
+const generateHTML = require('./generateHTML');
+const fs = require('fs')
+const inquirer = require('inquirer')
 const generatePDF = require('html-pdf')
 const axios = require('axios')
 
 // define data parameters
 const dataParams = {
+    profile: '',
     favcolor: '',
     avi: '',
     realName: '',
@@ -19,7 +20,7 @@ const dataParams = {
     followers: '',
     stars: '',
     following: ''
-}
+};
 
 // begin promise
 // const writeFileAsync = util.promisify(fs.writeFile)
@@ -29,18 +30,15 @@ const dataParams = {
 //     return inquirer.prompt(generateHTML.questions)
 // }
 
+// function for creating the pdf file
 function createFile(pdfName, html) {
     console.log('Please wait')
-    fs.writeFile(pdfName, html, (err) => {
-        if (err) throw err;
-    })
+    fs.writeFile(pdfName, html)
 
-    const options = { format: 'A4' }
+    const options = { format: 'A3' }
 
-    generatePDF.create(html, options).toFile('./profile.pdf', function(err, res) {
-        if (err) return console.log(err);
-        console.log('PDF generated');
-    })
+    generatePDF.create(html, options).toFile(`./${dataParams.profile}.pdf`)
+    console.log('PDF generated');
 }
 
 // display prompts on console
@@ -58,33 +56,38 @@ function createFile(pdfName, html) {
 
 function init() {
     // ask the questions
-    inquirer.prompt(generateHTML.questions).then(
-        function(data) {
-            const htmlName = 'profile.html'
-                // link answers to HTML data
-            dataParams.favcolor = data.color
-            dataParams.profile = data.username
+    inquirer.prompt(generateHTML.questions).then((data) => {
+        const htmlName = `${data.username}.html`
+            // link answers to HTML data
+        dataParams.favcolor = data.color
+        dataParams.profile = data.username
 
-            // pull data from github and link to my parameters
-            axios.get(`https://api.github.com/users/${data.username}`).then(function(response) {
-                if (response.status == 200) {
-                    dataParams.avi = response.data.avatar_url
-                    dataParams.realName = response.data.name
-                    dataParams.business = response.data.company
-                    dataParams.location = response.data.location
-                    dataParams.profileLink = response.data.html_url
-                    dataParams.blog = response.data.blog
-                    dataParams.bio = response.data.bio
-                    dataParams.pubRepos = response.data.public_repos
-                    dataParams.followers = response.data.followers
-                    dataParams.stars = response.data.public_gists
-                    dataParams.following = response.data.following
-                        // insert data into pdf creator
-                    const newFile = generateHTML.generateHTML(dataParams);
-                    createFile(htmlName, newFile);
-                }
-            })
-        }
-    )
+        // pull data from github and link to my parameters
+
+
+        axios.get(`https://api.github.com/users/${data.username}`).then(function(response) {
+            if (response.status === 200) {
+                dataParams.avi = response.data.avatar_url
+                dataParams.realName = response.data.name
+                dataParams.business = response.data.company
+                dataParams.location = response.data.location
+                dataParams.profileLink = response.data.html_url
+                dataParams.blog = response.data.blog
+                dataParams.bio = response.data.bio
+                dataParams.pubRepos = response.data.public_repos
+                dataParams.followers = response.data.followers
+                dataParams.stars = response.data.public_gists
+                dataParams.following = response.data.following
+                    // insert data into pdf creator
+                    // console.log(data.color)
+            }
+        })
+        try {
+            const newFile = generateHTML.generateHTML(dataParams);
+            createFile(htmlName, newFile);
+        } catch (error) {
+            console.log("error caught:", error.name, error.message)
+        } finally { console.log(typeof newFile) }
+    })
 }
 init();
